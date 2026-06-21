f6da4ad8bf5d64abe99e7c8157c91e7dbc413eb8
kweonsikyung <ekrk6632@naver.com>
2026-06-22 01:26:50 +0900

    fix: middleware matcher에서 assets 경로 제외하여 이미지 경로 오류 수정

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,5 +4,5 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|fonts|og-image.png).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|fonts|og-image.png|assets).*)"],
 };
